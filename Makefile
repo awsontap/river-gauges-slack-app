50b1bcb9656af9ef784dfe16a7ef54bf0816c674
Jordan Bach <jordanbach@gmail.com>
2017-10-29 12:15:19 -0500

    add the slack echo command blueprint from the lambda console

--- Makefile
+++ Makefile
@@ -2,12 +2,26 @@ SHELL = /bin/bash
 
 deploy: build
 	aws cloudformation deploy \
+		--stack-name river-guages-app \
 		--template-file tmp/sam-template-output.yaml \
-		--stack-name river-guages-app
+		--capabilities CAPABILITY_IAM
 
-build:
+build: build/lambda build/sam
+
+build/lambda: clean
+	mkdir -p tmp/lambda_code && \
+	pip install requests -t tmp/lambda_code && \
+	cp guages.py tmp/lambda_code && \
+	cd tmp/lambda_code && \
+	zip -r9 ../lambda_deployment_package.zip .
+
+build/sam:
 	aws cloudformation package \
 		--template-file sam-template.yaml \
 		--s3-bucket aws-on-tap \
 		--s3-prefix river-guages-app \
 		--output-template-file tmp/sam-template-output.yaml
+
+clean:
+	rm tmp/lambda_deployment_package.zip && \
+	rm -rf tmp/lambda_code
--- guages.py
+++ guages.py
@@ -1,8 +1,21 @@
 import boto3
+import json
+import logging
+import os
 import requests
 import sys
 
+from base64 import b64decode
 from pprint import pprint
+from urlparse import parse_qs
+
+ENCRYPTED_EXPECTED_TOKEN = os.environ['kmsEncryptedToken']
+
+kms = boto3.client('kms')
+expected_token = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_EXPECTED_TOKEN))['Plaintext']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 favorite_gauges_table = 'river-guages-app-FavoriteGauges-1AY0P6LELBSWG'
@@ -14,10 +27,12 @@ def add_favorite_gauge(gauge_no, gauge_name):
         'GuageName': gauge_name
     })
 
+
 def list_favorite_gauges(consistent_read=False):
     table = dynamodb.Table(favorite_gauges_table)
     response = table.scan(ConsistentRead=consistent_read)
     return response['Items']
+
 
 def check_gauge(gauge_no):
     base_url = 'https://waterdata.usgs.gov/tx/nwis/uv'
@@ -26,6 +41,33 @@ def check_gauge(gauge_no):
     print "GET %s" % full_url
     return requests.get(full_url)
 
+
+def lambda_response(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    params = parse_qs(event['body'])
+    token = params['token'][0]
+    if token != expected_token:
+        logger.error("Request token (%s) does not match expected", token)
+        return lambda_response(Exception('Invalid request token'))
+
+    user = params['user_name'][0]
+    command = params['command'][0]
+    channel = params['channel_name'][0]
+    command_text = params['text'][0]
+
+    return lambda_response(None, "%s invoked %s in %s with the following text: %s" % (user, command, channel, command_text))
+
+
+# For running in a shell during local development/testing
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "usage: python gauges.py <add|check>"
