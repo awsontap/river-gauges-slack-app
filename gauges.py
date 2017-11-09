ae54a487c9d4cd30d8cadd921213b2f845489ba9
Jordan Bach <jordanbach@gmail.com>
2017-11-08 22:59:44 -0600

    a little more code cleanup

--- gauges.py
+++ gauges.py
@@ -10,70 +10,36 @@ from base64 import b64decode
 from pprint import pprint
 from urlparse import parse_qs
 
-ENCRYPTED_EXPECTED_TOKEN = os.environ['kmsEncryptedToken']
-FAVORITE_GAUGES_TABLE = os.environ['favoriteGaugesTable']
-
-if ENCRYPTED_EXPECTED_TOKEN != 'local':
-    kms = boto3.client('kms')
-    expected_token = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_EXPECTED_TOKEN))['Plaintext']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-dynamodb = boto3.resource('dynamodb')
 
-def add_favorite_gauge(params, match):
-    gauge_no = match.group(1)
-    gauge_name = match.group(2)
-    table = dynamodb.Table(FAVORITE_GAUGES_TABLE)
-    table.put_item(Item={
-        'USGSSiteNumber': gauge_no,
-        'GuageName': gauge_name
-    })
-    return lambda_response(None, "added gauge %s %s" % (gauge_no, gauge_name))
+def lambda_handler(event, context):
+    """Entrypoint into the lambda function."""
+    params = parse_qs(event['body'])
+    token = params['token'][0] if 'token' in params else ''
 
+    if not verify_slack_token(token):
+        logger.error("Request token (%s) does not match expected token", token)
+        return lambda_response(Exception('Invalid request token'))
 
-def list_favorite_gauges(params, match):
-    table = dynamodb.Table(FAVORITE_GAUGES_TABLE)
-    response = table.scan()
-    # return response['Items']
-    gauge_list = ["%12s  %s" % (item['USGSSiteNumber'], item['GuageName']) for item in response['Items']]
-    return lambda_response(None, {
-        "text": """
-Your Favorite River Guages
---------------------------
-%s
-""".strip() % "\n".join(gauge_list)
-        })
+    return gauges_app(params)
 
 
-def check_gauge(params, match):
-    gauge_no = match.group(1)
+def lambda_response(err, res=None):
+    """Return a lambda response suitable for API gateway."""
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
 
-    stats_url = 'https://waterdata.usgs.gov/nwis/uv?cb_00060=on&format=rdb&site_no=%s&period=1' % gauge_no
-    graph_url = 'https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=%s&parm_cd=00060&period=1&format=gif_stats' % gauge_no
-
-    response = requests.get(stats_url)
-    last_measurement = response.text.strip().split("\n")[-1]
-    logger.info("stats_url=\"%s\"" % last_measurement)
-    logger.info("last_measurement=\"%s\"" % last_measurement)
-    _, _, _, mtime, tz, cfs, _ = re.split('\s+', last_measurement)
-
-    return lambda_response(None, {
-        "text": "Last measurement: %s cfs @ %s %s" %  (cfs, mtime, tz),
-        "attachments": [{ "image_url":  graph_url }]
-    })
-
-def display_help_message():
-    return lambda_response(None, {
-        "text":  """
-/gauges list - list favorite gauges
-/gauges add USGS_SITE_NUMBER RIVER_DESCRIPTION - add gauge to list of favorite gauges
-/gauges check USGS_SITE_NUMBER - display current flow readings for gauge
-        """.strip(),
-    })
 
 def gauges_app(params):
+    """Parse the /gauges Slack command and invoke the corresponding function."""
     user = params['user_name'][0]
     command = params['command'][0]
     channel = params['channel_name'][0]
@@ -92,24 +58,81 @@ def gauges_app(params):
 
     return display_help_message()
 
-def lambda_response(err, res=None):
-    return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
+
+def add_favorite_gauge(params, match):
+    """Add gauge to dynamodb table."""
+    gauge_no = match.group(1)
+    gauge_name = match.group(2)
+    table = get_gauges_table()
+    table.put_item(Item={
+        'USGSSiteNumber': gauge_no,
+        'GuageName': gauge_name
+    })
+    return lambda_response(None, "added gauge %s %s" % (gauge_no, gauge_name))
 
 
-def lambda_handler(event, context):
-    params = parse_qs(event['body'])
-    token = params['token'][0]
-    if ENCRYPTED_EXPECTED_TOKEN != 'local' and token != expected_token:
-        logger.error("Request token (%s) does not match expected", token)
-        return lambda_response(Exception('Invalid request token'))
+def list_favorite_gauges(params, match):
+    """List gauges in dynamodb table."""
+    table = get_gauges_table()
+    response = table.scan()
+    gauge_list = ["%12s  %s" % (item['USGSSiteNumber'], item['GuageName']) for item in response['Items']]
+    return lambda_response(None, {
+        "text": """
+Your Favorite River Guages
+--------------------------
+%s
+""".strip() % "\n".join(gauge_list)
+        })
 
-    return gauges_app(params)
+
+USGS_STATS_URL_TEMPLATE = 'https://waterdata.usgs.gov/nwis/uv?cb_00060=on&format=rdb&site_no=%s&period=1' 
+USGS_GRAPH_URL_TEMPLATE = 'https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=%s&parm_cd=00060&period=1&format=gif_stats' 
+
+def check_gauge(params, match):
+    """Fetch and display gauge information from USGS Water Data."""
+    gauge_no = match.group(1)
+    stats_url = USGS_STATS_URL_TEMPLATE % gauge_no
+    graph_url = USGS_GRAPH_URL_TEMPLATE % gauge_no
+
+    response = requests.get(stats_url)
+    last_measurement = response.text.strip().split("\n")[-1]
+    _, _, _, mtime, tz, cfs, _ = re.split('\s+', last_measurement)
+
+    return lambda_response(None, {
+        "text": "Last measurement: %s cfs @ %s %s" %  (cfs, mtime, tz),
+        "attachments": [{ "image_url":  graph_url }]
+    })
+
+
+def display_help_message():
+    """Display usage information for the /gauges Slack command."""
+    return lambda_response(None, {
+        "text":  """
+/gauges list - list favorite gauges
+/gauges add USGS_SITE_NUMBER RIVER_DESCRIPTION - add gauge to list of favorite gauges
+/gauges check USGS_SITE_NUMBER - display current flow readings for gauge
+        """.strip(),
+    })
+
+
+def get_gauges_table():
+    """Helper function to return a dynamodb table resource."""
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['favoriteGaugesTable']
+    return dynamodb.Table(table_name)
+
+
+def verify_slack_token(request_token):
+    """Ensure the Slack token given in request matches token provided by Slack."""
+    encrypted_token = os.environ['kmsEncryptedToken']
+
+    if encrypted_token == 'local':
+        return True
+
+    kms = boto3.client('kms')
+    expected_token = kms.decrypt(CiphertextBlob=b64decode(encrypted_token))['Plaintext']
+
+    return request_token == expected_token
 
 
 # For running in a shell during local development/testing
